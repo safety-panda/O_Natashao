6a5c82cab41918e4dcdd8cb50a54f6a4a5eccad8
natashadotpy <earmiffs+git@googlemail.com>
2021-03-01 14:04:43 +0000

    added starting board token set up

--- Form1.cs
+++ Form1.cs
@@ -61,20 +61,26 @@ namespace O_Natashao
             {
                 for (int col = 0; col <= 7; col++)
                 {
-                    if (((row == x) && (col == x)) || ((row == x) && (col == x)))
+                    if (((row == 3) && (col == 3)) || ((row == 4) && (col == 4)))
                     {
+                        // sets player one starting tokens 
+                        // WHITE tokens
                         checkerBoard[row, col] = 1;
                     }
-                    else if (((row == x) && (col == x)) || ((row == x) && (col == x)))
+                    else if (((row == 3) && (col == 4)) || ((row == 4) && (col == 3)))
                     {
+                        // sets player two staring tokens
+                        // BLACK tokens
                         checkerBoard[row, col] = 0;
                     }
                     else
                     {
+                        // sets all other game spaces to blank
                         checkerBoard[row, col] = 10;
                     }
                 }
             }
+            return checkerBoard;
         }
 
         private void Which_Element_Clicked(object sender, EventArgs e)
@@ -87,8 +93,7 @@ namespace O_Natashao
         {
             string path = Directory.GetCurrentDirectory() + "\\images\\";
 
-
-            infinateBoard();
+            startingBoard();
 
             GCheckerBoard = new GImageArray(this, checkerBoard, 50, 50, 100, 50, 0, path);
             GCheckerBoard.Which_Element_Clicked += new GImageArray.ImageClickedEventHandler(Which_Element_Clicked);
@@ -97,7 +102,7 @@ namespace O_Natashao
         // this button outputs the array values into a text box
         private void button1_Click(object sender, EventArgs e)
         {
-            infinateBoard();
+        //    infinateBoard();
 
             for (int row = 0; row <= 7; row++)
             {
